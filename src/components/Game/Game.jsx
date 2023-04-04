67f3dae4c855a8975353aa19bc0ce3d0dbcf8bcf
Nico <nicodiazan@gmail.com>
2023-04-04 20:00:27 +0200

    Add onclick generation

--- src/components/Game/Game.jsx
+++ src/components/Game/Game.jsx
@@ -19,6 +19,11 @@ class Game {
     game;
     setGame;
 
+    currPlayer;
+    setCurrPlayer;
+
+    onClicks;
+
     //size is an array [width, height]
     //players is an array of two player objects
     //a player object is such that {symbol: "X", isPlayer: true}
@@ -29,7 +34,47 @@ class Game {
             players: players,
         });
 
+        [this.currPlayer, this.setCurrPlayer] = useState(players[0]);
+
         useEffect(this.#checkWinState, [this.game.board]);
+
+        this.onClicks = this.#getOnClicks();
+    }
+
+    #getOnClicks () {
+        const onClicks = [];
+
+        for (let col = 0; col < this.game.size[0]; col++) {
+            onClicks.push([]);
+            for (let row = 0; row < this.game.size[1]; row++) {
+                onClicks[col].push(
+                    () => {
+                        this.#setCell(
+                            {
+                                game: this.game,
+                                setGame: this.setGame,
+                                col: col,
+                                row: row,
+                                player: this.currPlayer
+                            }
+                        )
+                    }
+                )
+                
+            }
+        }
+    }
+
+    #setCell({game: game, setGame: setGame, row, col, player}) {
+        const newGame = game;
+
+        const newBoard = game.board;
+
+        newBoard[col][row] = player.symbol;
+
+        newGame = {...game, board: newBoard};
+
+        setGame(newGame);
     }
 
     #checkWinState() {
